a777b6fd0cad645ee49242dfb641256cf8b78dc6
Gail Hobbs <gbhobbs97@gmail.com>
2023-04-13 09:13:25 -0700

    completed tables

--- server/models.py
+++ server/models.py
@@ -11,3 +11,47 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    serialize_rules = ('-restaurant_pizzas',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', 'restaurant')
+    pizzas = association_proxy('restaurant_pizzas', 'pizza')
+
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    serialize_rules = ('-restaurants', '-pizzas', '-created_at','-updated_at')
+
+    id = db.Column(db.Integer, primary_key=True)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurant = association_proxy('restaurants', 'restaurant')
+
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+
+    serialize_rules = ('-restaurant_pizzas', '-created_at','-updated_at')
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', 'pizza')
+    restaurants = association_proxy('restaurant_pizzas', 'restaurant')
+
